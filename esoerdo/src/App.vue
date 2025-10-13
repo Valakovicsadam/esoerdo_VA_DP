8c4421e26e451c438140453f3212fb6696b2702b
Dittmann Péter <dittmann.peter.2021i@bankitatabanya.hu>
2025-10-13 13:56:21 +0200

    Update App.vue

--- esoerdo/src/App.vue
+++ esoerdo/src/App.vue
@@ -5,7 +5,7 @@ import Rainforest from './components/Rainforest.vue';
 </script>
 
 <template>
-<<<<<<< HEAD
+
 <header>
 
 </header>
@@ -15,9 +15,7 @@ import Rainforest from './components/Rainforest.vue';
 <footer>
 
 </footer>
-=======
-    
->>>>>>> f84a4c0d4d84981f5ed1bfd87317bdac0f5bf116
+
 </template>
 
 <style scoped>
