5c640ddc75db3d3da5cb385294c31432702271aa
Valakovicsadam <valakovics.adam.2021i@bankitatabanya.hu>
2025-10-20 20:56:54 +0200

    Alapok feltöltése

--- esoerdo/src/components/PlantAndAnimals.vue
+++ esoerdo/src/components/PlantAndAnimals.vue
@@ -1,20 +1,34 @@
 <script setup>
+
 import { ref } from 'vue';
-import {animals, plants} from '../data/PlantsAndAnimals.js';
-const animals = ref(animals)
-const plants = ref(plants)
+import { animals } from '../data/PlantsAndAnimals';
+const allatok = ref(animals)
+
 
 </script>
 
 <template>
-    <header></header>
-    <section>
-        
-    </section>
-    <footer></footer>
-</template>
+    <template>
+        <section>
+            <div class="gallery">
+                <h1 class="title">Animals Gallery</h1>
+                <div class="cards">
+                    <div class="card" v-for="animal in allatok" :key="animal.name">
+                    
+                        <h2 class="card-title">{{ animal.name }}</h2>
+                        <p class="card-description">{{ animal.description }}</p>
+                    </div>
+                </div>
+            </div>
+        </section>
 
+    </template>
+</template>
 <style scoped>
+
+.section {
+   padding-top: 300px;
+}
 
 
 </style>
